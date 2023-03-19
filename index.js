56f0f19a92fbc22fcc8f13ee197aa1cc555ee862
Juma Musa <juma.musa@moringaschool.com>
2023-03-19 17:32:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"]; 
+  
+ function writeCards(names,event)  
+ { 
+   const newArray = [] 
+    
+   for (let i = 0; i < names.length; i++) 
+ { 
+  
+   const cardMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!` 
+   newArray.push(cardMessage) 
+    
+ } 
+  
+ return newArray; 
+ } 
+      
+  
+ writeCards("names", "surprise"); 
+  
+  
+  
+ function countDown() { 
+   var countdown = 11 
+ while (countdown > 0){ 
+     countdown--; 
+     console.log(countdown); 
+ } 
+ }
